c5bcd9556ec1eb5907f38ddb94fc8cfd75e5df02
Parvesh Kumar <parvesh427620@gmail.com>
2026-09-05 19:14:37 +0530

    Next.js added to the Project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="relative min-h-screen w-full overflow-hidden bg-black">
+      <img
+        src="/KS_logo.png"
+        alt="Koscon Studios"
+        className="absolute inset-0 h-full w-full object-cover object-center"
+      />
+    </main>
+  );
+}
